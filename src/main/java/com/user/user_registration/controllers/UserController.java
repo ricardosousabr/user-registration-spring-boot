f460fddb09574bbbff1dae75e2a9f013e389124f
ricardosousabr <ricardo.br.pi@gmail.com>
2025-11-12 23:39:20 -0300

    perf: update in controller

--- src/main/java/com/user/user_registration/controllers/UserController.java
+++ src/main/java/com/user/user_registration/controllers/UserController.java
@@ -21,17 +21,17 @@ public class UserController {
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<User> getAllUser() {
 		return userService.getAllUser();
-	}
+	} // ADMIN
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public User getUser(@PathVariable Long id){
 		return userService.getUser(id);
 	}
 	
-	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public User createNewUser(@RequestBody User user) {
-		return userService.createNewUser(user);
-	}
+//	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+//	public User createNewUser(@RequestBody User user) {
+//		return userService.createNewUser(user);
+//	}
 	
 	@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public User updateUser(@PathVariable Long id, @RequestBody User user) { return userService.updateUser(id, user); }
