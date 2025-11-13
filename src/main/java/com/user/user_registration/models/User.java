4860e3bfe4a87e74eb6800a4c3aecc15c2a9ac10
ricardosousabr <ricardo.br.pi@gmail.com>
2025-11-12 23:25:40 -0300

    feat: update in table user

--- src/main/java/com/user/user_registration/models/User.java
+++ src/main/java/com/user/user_registration/models/User.java
@@ -5,8 +5,16 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @Setter
 @Getter
@@ -14,7 +22,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "user")
 @EqualsAndHashCode(of = "id")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,12 +30,72 @@ public class User implements Serializable {
 	@Column(nullable = false)
 	private String name;
 	
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String email;
+	
+	@Column(nullable = false)
+	private String password;
 	
 	@Column(nullable = false)
 	private Integer age;
 	
-	public User() {}
+	@Column(nullable = false)
+	private UserRole role;
 	
+	@UpdateTimestamp
+	@Column(updatable = false, name = "created_at")
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "upadated_at")
+	private Date updatedAt;
+	
+	public User() {
+	
+	}
+	
+	public User(String name, Integer age, String login, String password, UserRole role ) {
+		this.name = name;
+		this.age = age;
+		this.email = login;
+		this.password = password;
+		this.role = role;
+		
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+		else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+	
+	@Override
+	public String getPassword() {
+		return password;
+	}
+	
+	@Override
+	public String getUsername () {
+		return email;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
