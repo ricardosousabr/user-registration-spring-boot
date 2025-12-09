737a2f7ebe12436c33fb5e54320d59dc1876986e
ricardosousabr <ricardo.br.pi@gmail.com>
2025-12-08 23:24:45 -0300

    feat: add an exception if the user is not found.

--- src/main/java/com/user/user_registration/service/AuthorizationService.java
+++ src/main/java/com/user/user_registration/service/AuthorizationService.java
@@ -15,6 +15,7 @@ public class AuthorizationService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return repository.findByEmail(username);
+		return repository.findByEmail(username)
+				.orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 	}
 }
