d1155a3bfdb09d69e1055cfc3881ff947ac95158
ricardosousabr <ricardo.br.pi@gmail.com>
2025-12-08 23:21:57 -0300

    feat: update in route paths

--- src/main/java/com/user/user_registration/configs/SecurityConfig.java
+++ src/main/java/com/user/user_registration/configs/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.user.user_registration.configs;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,9 +26,9 @@ public class SecurityConfig {
 				.csrf(AbstractHttpConfigurer::disable)
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
-						.requestMatchers(HttpMethod.POST, "auth/login").permitAll()
-						.requestMatchers(HttpMethod.POST, "auth/register").permitAll()
-						.requestMatchers(HttpMethod.DELETE, "/user").hasRole("ADMIN")
+						.requestMatchers( "/auth/login", "/auth/register").permitAll()
+						.requestMatchers( "/auth/update", "/auth/delete/**").authenticated()
+						.requestMatchers( "/user").hasRole("ADMIN")
 						.anyRequest().authenticated()
 				)
 				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
